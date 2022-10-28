7e25381d0ab2da9c64576d3b0f17f821d10be045
posite <su84053@gmail.com>
2022-10-29 00:24:47 +0900

    retrofit2를 이용한 카카오맵 intent 완전 구현

--- app/src/main/java/com/sys/test/KakaoMapApi.kt
+++ app/src/main/java/com/sys/test/KakaoMapApi.kt
@@ -6,5 +6,5 @@ import retrofit2.http.Query
 
 interface KakaoMapApi {
     @GET("searchList?apiKey=rrq71a2rotyj9tqm&locale=kr&page=1")
-    fun getData() : Call<Item>
+    fun getData() : Call<monttak>
 }
--- app/src/main/java/com/sys/test/MainActivity.kt
+++ app/src/main/java/com/sys/test/MainActivity.kt
@@ -16,41 +16,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        var longitude : Double = 0.0
+        var latitude : Double  = 0.0
         val view = binding.root
         setContentView(view)
         val retrofit = Retrofit.Builder().baseUrl("https://api.visitjeju.net/vsjApi/contents/").addConverterFactory(GsonConverterFactory.create()).build()
         val api = retrofit.create(KakaoMapApi::class.java)
         val kakaoMap = api.getData()
-        kakaoMap.enqueue(object : Callback<Item>{
-            override fun onResponse(call: Call<Item>, response: Response<Item>)
+        kakaoMap.enqueue(object : Callback<monttak>{
+            override fun onResponse(call: Call<monttak>, response: Response<monttak>)
             {
                 if(response.isSuccessful && response.code() == 200){
-                    api?.let{
-
-                    }
+                    val data = response.body()!!
+                    binding.button.text=data.items[1].title
+                    longitude = data.items[1].longitude
+                    latitude = data.items[1].latitude
                     Log.d("결과", "성공 : ${response.raw()}")
                 }
 
             }
-            override fun onFailure(call: Call<Item>, t: Throwable) {
+            override fun onFailure(call: Call<monttak>, t: Throwable) {
                 Log.d("결과:", "실패 : $t")
             }
         })
 
 
         binding.button.setOnClickListener {
-            val url ="kakaomap://route?sp=37.537229,127.005515&ep=37.4979502,127.0276368&by=FOOT"
-            var intent =  Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            intent.addCategory(Intent.CATEGORY_BROWSABLE)
-            var list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+            if(latitude != 0.0 && longitude != 0.0){
+                val url ="kakaomap://look?p=${latitude},${longitude}"
+                var intent =  Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                intent.addCategory(Intent.CATEGORY_BROWSABLE)
+                var list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
 
-            //카카오맵 어플리케이션이 사용자 핸드폰에 깔려있으면 바로 앱으로 연동
-            //그렇지 않다면 다운로드 페이지로 연결
+                //카카오맵 어플리케이션이 사용자 핸드폰에 깔려있으면 바로 앱으로 연동
+                //그렇지 않다면 다운로드 페이지로 연결
 
-            if (list== null){
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=net.daum.android.map")))
-            }else{
-                startActivity(intent)
+                if (list== null){
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=net.daum.android.map")))
+                }else{
+                    startActivity(intent)
+                }
             }
         }
     }
